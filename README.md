ba992141b87e3ceb35ac0da22d7dfb0fcff6d836
Robert C Anderson <robtanderson4@gmail.com>
2015-10-29 11:11:59 -0500

    init: Initial commit for portfolio

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+
+
 # Portfolio
-A portfolio of FEWD projects
+
+A customized [Bootstrap](http://getbootstrap.com/) porfolio to showcase some of my personal and professional projects. 
+
+### Tools used on this project:
+<ul>
+<li>HTML5</li>
+<li> CSS3</li>
+<li> Bootstrap</li>
+<li> Gulp</li>
+<li> Responsive Design</li>
+</ul>
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+	minifyCSS = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify');
+	imagemin = require('gulp-imagemin');
+	pngquant = require('imagemin-pngquant');
+
+// Minify CSS and store the minified CSS files in the dist folder
+gulp.task('styles', function(){
+	gulp.src('src/css/*.css')
+		.pipe(minifyCSS())
+		.pipe(rename(function(path){
+			path.extname = ".min";
+		}))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(notify({message: 'Gulp/Styles complete!'}));
+});
+
+// Minify images and store the minified versions in the dist folder
+gulp.task('images', function(){
+	gulp.src('src/img/*')
+		.pipe(imagemin({
+			progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/img'))
+		.pipe(notify({message: 'Gulp/Images complete!'}));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/css/*.css', ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch', 'images']);
